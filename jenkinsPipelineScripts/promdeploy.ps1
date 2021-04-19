c85e356c6cb236c6bfaa86b468280e62d6f85f7c
Yaniv Eran <yeran@varonis.com>
2021-04-19 22:22:48 +0300

    .

--- jenkinsPipelineScripts/promdeploy.ps1
+++ jenkinsPipelineScripts/promdeploy.ps1
@@ -1,7 +1,7 @@
 $solr_pipeline_home=$args[0]
 
 cd $solr_pipeline_home
-pwd
+#pwd
 #git --version
-#git clone https://github.com/stefanprodan/dockprom
+git clone https://github.com/stefanprodan/dockprom
 #Copy-Item -Path "${SOLR_PIPELINE_HOME}\dockerpromModification\GrafanaDashboardExtra\*.json" -Destination "${SOLR_PIPELINE_HOME}\dockprom\grafana\provisioning\dashboards" -Force -Verbose
