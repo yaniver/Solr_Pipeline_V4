a59b31ac9328fe175e92ebf50fb99afac9c8521b
Yaniv Eran <yeran@varonis.com>
2021-04-27 20:34:53 +0300

    .

--- jenkinsPipelineScripts/solrExporterDeploy.ps1
+++ jenkinsPipelineScripts/solrExporterDeploy.ps1
@@ -14,7 +14,7 @@ $container_name="solrexporter"
 $container_solr_exporter_path="/opt/solr-8.8.2/contrib/prometheus-exporter/"
 $container_solr_exporter_script_path=$container_solr_exporter_path + $script_name
 
-$container_target_script=$container_name + ":" + $container_solr_exporter_script_path + $script_name
+$container_target_script=$container_name + ":" + $container_solr_exporter_script_path
 
 $container_target_config_core=$container_name + ":" + $container_solr_exporter_path + "conf/" + $solr_exporter_config_core
 $container_target_config_other=$container_name + ":" + $container_solr_exporter_path + "conf/" + $solr_exporter_config_other
