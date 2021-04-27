b7e864a422ccb8ff4adc73b572811285078bf6e1
Yaniv Eran <yeran@varonis.com>
2021-04-27 13:58:11 +0300

    .

--- SolrExporter/solrexporter-entrypoint.sh
+++ SolrExporter/solrexporter-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-./bin/solr-exporter -p 8094 -z http://10.10.193.95:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_core.xml -n 16 & disown
-#./bin/solr-exporter -p 8095 -z http://10.10.193.95:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_jvm.xml -n 16 & disown
-#./bin/solr-exporter -p 8096 -z http://10.10.193.95:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_Jetty_Node_ClusterStatus_OverseerStatus.xml -n 16 & disown
+./bin/solr-exporter -p 8094 -z http://10.10.193.92:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_core.xml -n 16 & disown
+#./bin/solr-exporter -p 8095 -z http://10.10.193.92:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_jvm.xml -n 16 & disown
+#./bin/solr-exporter -p 8096 -z http://10.10.193.92:2181/solr -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_Jetty_Node_ClusterStatus_OverseerStatus.xml -n 16 & disown
+
 
--- jenkinsPipelineScripts/solrExporterDeploy.ps1
+++ jenkinsPipelineScripts/solrExporterDeploy.ps1
@@ -29,10 +29,11 @@ $file_content -match $string_to_search
 
 $solrExpoExist=$(docker ps -f name=$container_name)
 if($solrExpoExist) {            
-    Write-Host "Your string is not EMPTY, meaning solrexporter container exist"           
-} else {            
+    Write-Host "Solr exporter container does not exist, start creating the container"
 	docker run --name $container_name --network=dockprom_monitor-net -d --restart always -p 8094:8094 -p 8095:8095 -p 8096:8096 solr:8.8.2-slim
 	docker cp $script_name $container_target_script
 	docker cp $solr_exporter_config_core $container_target_config_core
 	docker exec $container_name $container_solr_exporter_script_path
+} else {            
+	Write-Host "Solr exporter container already exist so no need to create additional container"
 }
