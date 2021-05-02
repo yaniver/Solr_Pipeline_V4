9acbd63027f2d3818cc4b3b32cd9e7299e2f6a69
Yaniv Eran <yeran@varonis.com>
2021-05-02 15:18:22 +0300

    .

--- SolrExporter/solrexporter-entrypoint.sh
+++ SolrExporter/solrexporter-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Hello World"
-#./bin/solr-exporter -p 8094 -z 10.10.193.92:2181 -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_core.xml -n 16
+./bin/solr-exporter -p 8094 -z 10.10.193.92:2181 -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_core.xml -n 16
 #./bin/solr-exporter -p 8095 -z 10.10.193.92:2181 -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_jvm.xml -n 16
 #./bin/solr-exporter -p 8096 -z 10.10.193.92:2181 -f /opt/solr-8.8.2/contrib/prometheus-exporter/conf/solr-exporter-config_Jetty_Node_ClusterStatus_OverseerStatus.xml -n 16
