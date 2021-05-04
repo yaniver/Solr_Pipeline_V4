c06c61b19d9107d91430b10e3b334fbead7659ec
Yaniv Eran <yeran@varonis.com>
2021-05-04 18:16:33 +0300

    .

--- jenkinsPipelineScripts/promdeploy.ps1
+++ jenkinsPipelineScripts/promdeploy.ps1
@@ -3,7 +3,7 @@ $solr_pipeline_home=$args[0]
 cd $solr_pipeline_home
 git clone https://github.com/stefanprodan/dockprom
 Copy-Item -Path "${SOLR_PIPELINE_HOME}\dockerpromModification\GrafanaDashboardExtra\*.json" -Destination "${SOLR_PIPELINE_HOME}\dockprom\grafana\provisioning\dashboards" -Force -Verbose
-#Copy-Item -Path "${SOLR_PIPELINE_HOME}\dockerpromModification\GrafanaDatasource\*.yml" -Destination "${SOLR_PIPELINE_HOME}\dockprom\grafana\provisioning\datasources" -Force -Verbose
+Copy-Item -Path "${SOLR_PIPELINE_HOME}\dockerpromModification\GrafanaDatasource\*.yml" -Destination "${SOLR_PIPELINE_HOME}\dockprom\grafana\provisioning\datasources" -Force -Verbose
 Copy-Item -Path "${SOLR_PIPELINE_HOME}\dockerpromModification\Prometheus\*.yml" -Destination "${SOLR_PIPELINE_HOME}\dockprom\prometheus" -Force -Verbose
 cd dockprom
 $ADMIN_USER="admin"
