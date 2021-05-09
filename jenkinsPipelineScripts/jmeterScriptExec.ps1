653f0bef2244ee383d5d2cd2752104f565161050
Yaniv Eran <yeran@varonis.com>
2021-05-09 18:37:03 +0300

    .

--- jenkinsPipelineScripts/jmeterScriptExec.ps1
+++ jenkinsPipelineScripts/jmeterScriptExec.ps1
@@ -9,16 +9,6 @@ $container_name="jmeter"
 cd $jmeter_full_path
 
 docker build -t $container_name .
-docker run --name $container_name -p 8983:8983 $container_name
 
-# JENKINS_HOME="/var/lib/jenkins"
-# export timestamp=$(date +%Y%m%d_%H%M%S)
-# export volume_path=${JENKINS_HOME}/CICD/JMeter
-# export jmeter_path=/mnt/jmeter
-# sudo docker run --name jmeter -p 8983:8983  \
-# --volume "${volume_path}":${jmeter_path} \
-# jmeter -n -Jcsv_staticParams=${jmeter_path}/static_parameters.txt \
-# -Jmy_csv=${jmeter_path}/collectionsList.txt \
-# -JThreads=5 \
-# -t ${jmeter_path}/SolrJ.jmx \
-# -l ${jmeter_path}/tmp/result_${timestamp}.jtl -j ${jmeter_path}/tmp/jmeter_${timestamp}.log
+
+docker run --name $container_name -p 3182:3182  --volume ${jmeter_full_path}:/mnt/jmeter jmeter -n -Jcsv_staticParams=/mnt/jmeter/static_parameters.txt -Jmy_csv=/mnt/jmeter/collectionsList.txt -JThreads=1 -t /mnt/jmeter/SolrJ.jmx -l /mnt/jmeter/tmp/result_1.jtl -j /mnt/jmeter/tmp/jmeter_1.log
