abfebb94809bb229fcf805223b38dc294d378cfa
Yaniv Eran <yeran@varonis.com>
2021-05-19 16:25:32 +0300

    .

--- jenkinsPipelineScripts/jmeterScriptExec.ps1
+++ jenkinsPipelineScripts/jmeterScriptExec.ps1
@@ -30,4 +30,4 @@ cd $jmeter_full_path
 docker build -t $container_name .
 
 
-docker run --name $container_name -p 3182:3182  --volume ${jmeter_full_path}:/mnt/jmeter jmeter -n -Jcsv_staticParams=/mnt/jmeter/static_parameters.txt -Jmy_csv=/mnt/jmeter/searchbody_parameters.txt -JThreads=1 -t /mnt/jmeter/SOLR_SEARCH.jmx -l /mnt/jmeter/tmp/result_1.jtl -j /mnt/jmeter/tmp/jmeter_1.log
+docker run --name $container_name -p 3182:3182 --volume ${jmeter_full_path}:/mnt/jmeter jmeter -n -Jcsv_staticParams=/mnt/jmeter/static_parameters.txt -Jmy_csv=/mnt/jmeter/searchbody_parameters.txt -JThreads=1 -t /mnt/jmeter/SOLR_SEARCH.jmx -l /mnt/jmeter/tmp/result_1.jtl -j /mnt/jmeter/tmp/jmeter_1.log
