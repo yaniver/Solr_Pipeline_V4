5a0351aca14e49708eded07ae3c1ad884645fbdf
Yaniv Eran <yeran@varonis.com>
2021-07-01 09:58:38 +0300

    .

--- jenkinsPipelineScripts/Prerequisites.ps1
+++ jenkinsPipelineScripts/Prerequisites.ps1
@@ -16,12 +16,14 @@ Write-Host "Enable access to WinRM listener via HTTP in IDU server"
 Invoke-Command -Session $Session -ScriptBlock {
 	cmd.exe --% /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 }
-
 Write-Host "Enable access to WinRM listener via HTTP in DB server"
 Invoke-Command -Session $Session_db -ScriptBlock {
 	cmd.exe --% /c winrm set winrm/config/service @{AllowUnencrypted="true"}
 }
 
+$clm_connection_string_encoded = Invoke-Command -Session $Session -ScriptBlock {Select-Xml -Path "C:\Program Files (x86)\Varonis\DatAdvantage\CollectionManager\Varonis.CollectionManager.Service.exe.config" -XPath '/configuration/connectionStrings/EncryptedData' | ForEach-Object { $_.Node.InnerXML }}
+$clm_connection_string_deencoded = Invoke-Command -ScriptBlock {..\CryptoVaronis\vrnsCrypto.ps1 $clm_connection_string_encoded}
+Write-Host "Collection manager connection string value  = " $clm_connection_string_deencoded
 
 Remove-PSSession $Session
 Remove-PSSession $Session_db
